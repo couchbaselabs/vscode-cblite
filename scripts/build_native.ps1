3018da3fce9976c2494c4eeb8a594ace1e7c64a2
Jim Borden <jim.borden@couchbase.com>
2022-01-15 08:38:40 +0900

    Fix build script to use public source

--- scripts/build_native.ps1
+++ scripts/build_native.ps1
@@ -1,5 +1,17 @@
 Push-Location $PSScriptRoot/..
 
+$CBL_C_VERSION = "3.0.0-beta02"
+$CBL_C_VERSION_SHORT = "3.0.0"
+$DOWNLOAD_URL = "https://packages.couchbase.com/releases/couchbase-lite-c/$CBL_C_VERSION/couchbase-lite-c-enterprise-$CBL_C_VERSION-windows-x86_64.zip"
+
+Push-Location deps
+Invoke-WebRequest $DOWNLOAD_URL -OutFile cbl.zip
+Expand-Archive cbl.zip -DestinationPath $pwd
+Copy-Item -Recurse libcblite-$CBL_C_VERSION_SHORT/* .
+Remove-Item -Recurse libcblite-$CBL_C_VERSION_SHORT/*
+Remove-Item cbl.zip
+Pop-Location
+
 npm install
 npm run build-native
 
